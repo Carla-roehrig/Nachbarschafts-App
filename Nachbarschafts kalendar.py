39f6c6b3b16f61833912994696f429720d6da9a0
Carla-roehrig <carla.roehrig@icloud.com>
2025-05-19 12:13:15 +0200

    Create Nachbarschafts kalendar.py

---
+++ Nachbarschafts kalendar.py
@@ -0,0 +1,17 @@
+#Nachbarschafts-App
+print("Willkommen beim Nachbarschafts Kalendar, geben sie einfach nur ein Datum ein und ich zeige ihnen dann was dort stattfindet. Geben sie nun ihr Datum ein:")
+antwort=input()
+anlaesse =  {
+    
+    "2025-12-24" : "Weihnachten"
+    "2025-05-05" : "Flohmarkt"
+    "2025-02-14" : "Valentistag"
+    "2025-12-31" : "Silvester"
+    "2025-07-06" : "Dorffest"
+    
+}
+def aufrufen:
+    print(anlaesse[antwort])
+    
+    if antwort in anlaesse:
+        print("Am 
